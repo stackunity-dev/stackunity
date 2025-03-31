5ef0a9935ebdeae41106738acf2cf265e85823d5
Nûr <djedidinur@gmail.com>
2025-03-31 17:33:33 +0200

    'build'

---
+++ plugins/pinia-persistedState.client.ts
@@ -0,0 +1,6 @@
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
+import { defineNuxtPlugin } from '#app';
+
+export default defineNuxtPlugin(({ $pinia }) => {
+  $pinia.use(piniaPluginPersistedstate);
+}); 
