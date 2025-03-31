1de8f9e73acadd9938e5b353249f6e89ba879815
Nûr <djedidinur@gmail.com>
2025-03-31 16:26:09 +0200

    release

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,10 +14,6 @@ export default defineNuxtConfig({
     externals: {
       inline: ['@pinia-plugin-persistedstate/nuxt'],
       external: ['puppeteer-core', '@puppeteer/browsers', 'monaco-editor']
-    },
-    server: {
-      port: process.env.PORT || 8080,
-      host: '0.0.0.0'
     }
   },
 
