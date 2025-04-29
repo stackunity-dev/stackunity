df29039fc0264d0f2c80e2cbe4e4d48072cef32c
stackunity-dev <stackunity-dev@users.noreply.github.com>
2025-04-29 12:39:36 +0200

    'build'

--- server/api/contact/submit.ts
+++ server/api/contact/submit.ts
@@ -1,7 +1,7 @@
 import { defineEventHandler, readBody } from 'h3';
 import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY as string);
 
 export default defineEventHandler(async (event) => {
   const { name, email, subject, message } = await readBody(event);
