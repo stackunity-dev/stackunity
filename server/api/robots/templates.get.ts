93e5006f7ae41628860ba258de3e9c3ca23fddc8
stackunity-dev <stackunity-dev@users.noreply.github.com>
2025-05-04 15:39:49 +0200

    'build'

--- server/api/robots/templates.get.ts
+++
@@ -1,6 +0,0 @@
-import { defineEventHandler } from 'h3';
-import { ROBOTS_TEMPLATES } from '../../../utils/robots/utils';
-
-export default defineEventHandler(() => {
-  return ROBOTS_TEMPLATES;
-}); 
--- server/api/schema/templates.get.ts
+++
@@ -1,6 +0,0 @@
-import { defineEventHandler } from 'h3';
-import { SCHEMA_TEMPLATES } from '../../../utils/robots/utils';
-
-export default defineEventHandler(() => {
-  return SCHEMA_TEMPLATES;
-}); 
