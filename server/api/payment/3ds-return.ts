cefb86f2a0c75c52a3f00fa9b68ca7bb9bcd8a36
stackunity-dev <stackunity-dev@users.noreply.github.com>
2025-05-23 21:01:07 +0200

    'build'

--- server/api/payment/3ds-return.ts
+++ server/api/payment/3ds-return.ts
@@ -11,7 +11,6 @@ export default defineEventHandler(async (event) => {
   if (!token) return { success: false, error: 'Token de commande manquant' };
 
   const orderId = orderTokensMap[token];
-  console.log('orderId', orderId);
 
   if (!orderId) {
     return { success: false, error: 'Token invalide ou expiré' };
@@ -23,21 +22,22 @@ export default defineEventHandler(async (event) => {
     // Vérifie la commande avec l’orderId réel
     const orderRequest = new checkoutNodeJssdk.orders.OrdersGetRequest(orderId);
     const order = await paypal.execute(orderRequest);
-    console.log('order', order);
 
     if (order.result.status !== 'APPROVED') {
       return { success: false, error: `Commande non approuvée: ${order.result.status}` };
     }
 
     // Capture la commande avec l’orderId réel
-    const capture = await retryCapture(paypal);
+    const captureRequest = new checkoutNodeJssdk.orders.OrdersCaptureRequest(orderId);
+    captureRequest.requestBody({});
+    const capture = await paypal.execute(captureRequest);
 
     console.log('Capture response:', capture);
 
-    if (capture.success) {
+    if (capture.result.status === 'COMPLETED') {
       return { success: true, message: 'Paiement capturé avec succès après 3DS' };
     } else {
-      return { success: false, error: `Capture échouée : ${capture.error}` };
+      return { success: false, error: `Capture échouée : ${capture.result.status}` };
     }
   } catch (error: any) {
     console.error('Erreur lors du retour 3DS:', error);
