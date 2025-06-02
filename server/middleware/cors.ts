bdfa9b61eaee4278b7e92a56d55e83625cd6bcdc
stackunity-dev <stackunity-dev@users.noreply.github.com>
2025-06-02 19:30:48 +0200

    'build'

--- server/middleware/cors.ts
+++ server/middleware/cors.ts
@@ -1,13 +1,19 @@
-import { defineEventHandler } from 'h3';
+import { defineEventHandler } from 'h3'
 
 export default defineEventHandler((event) => {
-  event.node.res.setHeader('Access-Control-Allow-Origin', '*');
-  event.node.res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-  event.node.res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  const origin = event.node.req.headers.origin
+
+  if (origin) {
+    event.node.res.setHeader('Access-Control-Allow-Origin', origin)
+    event.node.res.setHeader('Access-Control-Allow-Credentials', 'true')
+  }
+
+  event.node.res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE')
+  event.node.res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
 
   if (event.node.req.method === 'OPTIONS') {
-    event.node.res.statusCode = 204;
-    event.node.res.end();
-    return;
+    event.node.res.statusCode = 204
+    event.node.res.end()
+    return
   }
-});
+})
